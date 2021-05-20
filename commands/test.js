620a4635bf8fc735703f4aa8995467f0ab8a399f
LilSheeps <54599946+LilSheeps@users.noreply.github.com>
2021-05-20 19:45:53 -0400

    Add files via upload

---
+++ commands/test.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  return message.channel.send('Hello, My name is Henzu. Im pretty retared and Tristan is trying to fix me. He is having massive migranes trying to fix me, he doesnt even know if you guys would even be able to see this message. So yeah, tristan wants to die HAHA');
+
+};
+
+module.exports.help = {
+  name: "test"
+};
