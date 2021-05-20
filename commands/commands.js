e1b359b112d15f52e4a3bef9335484f6be1e03e2
LilSheeps <54599946+LilSheeps@users.noreply.github.com>
2021-05-19 23:27:13 -0400

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  return message.channel.send('You can do commands such as funfact, henzusucks, afk, and ping using the ^ prefix! More commands will be added soon.');
+  return message.channel.send('You can do commands such as ^say, ^funfact, ^henzusucks, ^afk, and ^ping. More commands will be added soon.');
 
 };
 
