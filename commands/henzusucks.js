d7ea91552fc3d688f7342fd545f89fd0b236d3df
LilSheeps <54599946+LilSheeps@users.noreply.github.com>
2021-05-19 22:53:08 -0400

    Update henzusucks.js

--- commands/henzusucks.js
+++ commands/henzusucks.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  return message.channel.send('TRIGGERED');
+  return message.channel.send('Shut yo skin tone chicken bone google chrome no home flip phone disowned ice cream cone garden gnome extra chromosome metronome dimmadome genome full blown monochrome student loan indiana jones overgrown flintstone x and y hormone post malone friend zone sylvester stallone hydrocortisone sierra leone autozone professionally seen silver patrone head ass tf up');
 
 };
 
